8649fce38af752c266fede384003f727cf987cc6
Thiago Malta da Silva <seu_email_do_gitlab@exemplo.com>
2026-04-21 12:09:06 -0300

    feat: implementa ChatOperacoesInterface (Thiago)

---
+++ chat/domain/interfaces/teste.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+from domain.entities.usuario import Usuario
+from domain.entities.sala import Sala
+from domain.entities.mensagem import Mensagem
+
+
+class ChatOperacoesInterface(ABC):
+
+    @abstractmethod
+    def criar_novo_usuario(self, nome: str, senha: str) -> Usuario:
+        pass
+
+    @abstractmethod
+    def criar_nova_sala(self, nome: str) -> Sala:
+        pass
+
+    @abstractmethod
+    def listar_salas(self) -> list[Sala]:
+        pass
+
+    @abstractmethod
+    def enviar_mensagem(self, texto: str, id_sala: int, id_usuario: int) -> None:
+        pass
+
+    @abstractmethod
+    def listar_mensagens(self) -> list[Mensagem]:
+        pass
+
+    @abstractmethod
+    def apagar_mensagem(self, id_mensagem: int, id_usuario: int) -> None:
+        pass
