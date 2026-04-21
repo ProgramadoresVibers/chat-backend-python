4c62d02534f59a125322f77439f97f9684e5355e
Iarley Souza <iarleysouza615@gmail.com>
2026-04-21 16:54:36 -0300

    revert resultado

--- chat/domain/shared/resultado.py
+++ chat/domain/shared/resultado.py
@@ -1,16 +1,13 @@
-from typing import Generic, TypeVar, Optional
-
-T = TypeVar("T")
-class Resultado(Generic[T]):
-    def __init__(self, sucesso: bool, conteudo: Optional[T] = None, erro: str = None):
+class Resultado:
+    def __init__(self, sucesso, conteudo=None, erro=None):
         self.sucesso = sucesso
         self.conteudo = conteudo
         self.erro = erro
 
     @staticmethod
-    def ok(conteudo: T):
-        return Resultado(True, conteudo)
+    def ok(conteudo):
+        return Resultado(sucesso=True, conteudo=conteudo)
 
     @staticmethod
-    def falha(erro: str):
-        return Resultado(False, None, erro)
+    def falha(erro):
+        return Resultado(sucesso=False, erro=erro)
