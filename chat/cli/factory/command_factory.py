db1d87b0d7bc582ac0545e9f2c0ad7436dfef18e
Estêvan Silva <esteevan160@gmail.com>
2026-05-12 21:58:13 -0300

    agradeça

--- chat/cli/factory/command_factory.py
+++ chat/cli/factory/command_factory.py
@@ -1,12 +1,12 @@
-from chat.cli.commands.apagar_mensagem_command import ApagarMensagemCommand
-from chat.cli.commands.criar_sala_command import CriarSalaCommand
-from chat.cli.commands.criar_usuario_command import CriarUsuarioCommand
-from chat.cli.commands.entrar_sala_command import EntrarSalaCommand
-from chat.cli.commands.enviar_mensagem_command import EnviarMensagemCommand
-from chat.cli.commands.help_command import HelpCommand
-from chat.cli.commands.listar_mensagens_command import ListarMensagensCommand
-from chat.cli.commands.listar_salas_command import ListarSalasCommand
-from chat.cli.commands.login_command import LoginCommand
+from commands.apagar_mensagem_command import ApagarMensagemCommand
+from commands.criar_sala_command import CriarSalaCommand
+from commands.criar_usuario_command import CriarUsuarioCommand
+from commands.entrar_sala_command import EntrarSalaCommand
+from commands.enviar_mensagem_command import EnviarMensagemCommand
+from commands.help_command import HelpCommand
+from commands.listar_mensagens_command import ListarMensagensCommand
+from commands.listar_salas_command import ListarSalasCommand
+from commands.login_command import LoginCommand
 
 
 class CommandFactory:
--- chat/cli/main.py
+++ chat/cli/main.py
@@ -1,9 +1,9 @@
 import json
 
-from chat.cli.commands.command_invoker import CommandInvoker
-from chat.cli.controllers.chat_controller import ChatController
-from chat.cli.factory.command_factory import CommandFactory
-from chat.cli.services.local_chat_service import LocalChatService
+from commands.command_invoker import CommandInvoker
+from controllers.chat_controller import ChatController
+from factory.command_factory import CommandFactory
+from services.local_chat_service import LocalChatService
 
 
 def validar_argumento(argumento, uso):
@@ -228,3 +228,6 @@ def rodar_cli(controller):
 def main():
     controller = criar_controller()
     rodar_cli(controller)
+
+if __name__ == "__main__":
+    main()
